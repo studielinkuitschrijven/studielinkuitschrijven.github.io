65a34c40476fa68b98fd285af4a51001ce4ee380
studielinkuitschrijven <59831516+studielinkuitschrijven@users.noreply.github.com>
2020-01-13 19:58:29 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var http = require("http");
+
+var port = process.argv[2];
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+http.createServer(app).listen(port);
+
+
